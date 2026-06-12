d8f20d48ed9494ade2812fcf111bf62a884b0c62
Anusha Vemu <anushavemu95@gmail.com>
2026-06-12 21:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (89.70%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -4,33 +4,31 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(!root) return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int m=q.size();
-            vector<int>lev;
-            for(int i=0;i<m;i++){
-                TreeNode*curr=q.front();
-            q.pop();
-            lev.push_back(curr->val);
-            if(curr->left)
-            q.push(curr->left);
-            if(curr->right)
-            q.push(curr->right);
+        vector<vector<int>> res;
+        if (root == NULL) return res;
+        queue<pair<TreeNode*, int>> q;
+        q.push(make_pair(root, 0));
+        vector<int> level;
+        int currentlevel=0;
+        while (!q.empty()) {
+            auto p=q.front(); q.pop();
+            if (p.second == currentlevel) level.push_back(p.first->val);
+            else {
+                res.push_back(level);
+                level.clear();
+                level.push_back(p.first->val);
+                currentlevel = p.second;
             }
-            ans.push_back(lev);
+            if (p.first->left) q.push(make_pair(p.first->left, currentlevel+1));
+            if (p.first->right) q.push(make_pair(p.first->right, currentlevel+1));
         }
-        return ans;
-        
+        res.push_back(level);
+        return res;
     }
 };
