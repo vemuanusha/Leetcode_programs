25d6068b1da9aa974f20f1af5b983451fb9fea7a
Anusha Vemu <anushavemu95@gmail.com>
2026-06-17 19:50:25 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (54.28%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
@@ -1,50 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int depth(TreeNode* root){
-        if(root == NULL)return 0;
-		//return the maximum depth of the tree so to find the levels
-        return max(depth(root->right),depth(root->left))+1;
-    }
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-     if(root == NULL)return {};
-        int d = depth(root);
-		//initialise the empty levels inside a vector
-        vector<vector<int> > level(d,vector<int>({})); 
-        int i = 0;
-		//use a queue to maintain the tree nodes
-        queue<TreeNode*> q;
-		//push the root node with one element and decrement the level
+        vector<vector<int>>v;
+        if(!root) return v;
+        queue<TreeNode*>q;
         q.push(root);
-        level[d-1].push_back(root->val);
-        q.push(NULL);
-        d--;
-
-		// do work while queue is not empty
         while(!q.empty()){
-			// extract the node from the queue and remove it from the queue
-            TreeNode* temp = q.front();
-            q.pop();
-            
-			//if the node exist check its right and left node
-            if(temp){  
-				// and if the left and right node exist simply push the value to the vector
-                if(temp->left){
-                        q.push(temp->left);
-                        level[d-1].push_back(temp->left->val);
-                    }    
-                if(temp->right){
-                        q.push(temp->right);
-                        level[d-1].push_back(temp->right->val);
-                    }
-            }                 
-            else {
-			// to handle condition if NULL found at a node
-                if(q.empty()) break;
-                d--;
-                q.push(NULL);
-            }  
+            int levelNodes = q.size();
+            vector<int>lvl;
+            for(int i=1;i<=levelNodes;i++){
+                TreeNode*curr=q.front();
+                q.pop();
+                lvl.push_back(curr->val);
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            v.push_back(lvl);
         }
-		// now without reversing the vector you can simply return the answer
-        return level;
+        reverse(v.begin(),v.end());
+        return v;
     }
 };
